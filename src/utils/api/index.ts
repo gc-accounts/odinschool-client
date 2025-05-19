fc635a109c714fcf57ffb7b8e14c7d04c7c226a6
Lokesh Rana <lokeshrana9999@gmail.com>
2025-05-20 01:31:06 +0530

    fixed backend mixed content issue 4

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,7 +1,6 @@
 import { getCourses, getCourse } from './courses';
 
 
-const imagePrefix = import.meta.env.VITE_BACKEND_URL;
 
 
-export { getCourses, getCourse, imagePrefix };
+export { getCourses, getCourse };
