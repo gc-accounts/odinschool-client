615be63c37bf12106b5a05a379bbc6120cdb16e0
Lokesh Rana <lokeshrana9999@gmail.com>
2025-05-26 04:53:57 +0530

    vercel function added 5

--- api/graphql.ts
+++
@@ -1,9 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(
-  request: VercelRequest,
-  response: VercelResponse
-) {
-  // Redirect to the GraphQL endpoint
-  response.redirect(301, 'https://google.com');
-} 
