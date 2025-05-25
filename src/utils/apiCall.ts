63501d6fec1f2d66425bbe4ed6f9639b3c313c93
Lokesh Rana <lokeshrana9999@gmail.com>
2025-05-26 05:03:14 +0530

    vercel function added  debug

--- src/utils/apiCall.ts
+++ src/utils/apiCall.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const backendUrl = import.meta.env.VITE_IS_DEVELOPMENT === 'true' ? 'http://13.203.229.113:1337' : '/api';
+export const backendUrl = import.meta.env.VITE_IS_DEVELOPMENT === 'true' ? 'http://13.203.229.113:1337' : '';
 
 const axiosApi = axios.create({
     baseURL: backendUrl + '/graphql',
