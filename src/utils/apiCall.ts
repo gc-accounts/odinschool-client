f00bdbbb9a696d59d92d2e296a6ca663756f9846
Lokesh Rana <lokeshrana9999@gmail.com>
2025-05-26 05:52:14 +0530

    vercel redirect implemented again

--- src/utils/apiCall.ts
+++ src/utils/apiCall.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const backendUrl = import.meta.env.VITE_IS_DEVELOPMENT === 'true' ? '' : 'https://dawn-lake-1c80.lokeshrana9999.workers.dev';
+export const backendUrl = import.meta.env.VITE_IS_DEVELOPMENT === 'true' ? 'http://13.203.229.113:1337' : '';
 
 const axiosApi = axios.create({
     baseURL: backendUrl + '/graphql',
