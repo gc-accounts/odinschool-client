cd9c3cc0872bfd305da9f7a2011778e11982b47f
Sheikh-Fayyaz-OdinSchool <fayyaz@odinschool.com>
2025-06-02 16:38:07 +0530

    dsJobDrives Data

--- src/components/data/dsJobsDrives.ts
+++ src/components/data/dsJobsDrives.ts
@@ -1,7 +1,7 @@
 export const dsJobsDrives = [
   {
     "id": 1,
-    "jobCompanyLogo": "https://www.odinschool.com/hubfs/OS_Company_Logo/v2/100_X_40/quriousbit_logo%20100X40.webp",
+    "jobCompanyLogo": "https://strapi.odinschool.com/uploads/quriousbit_logo_20100_X40_85428525aa.webp",
     "jobDesignation": "Data Analyst",
     "jobExp": "Fresher",
     "jobSkills": ["SQL", "Python", "Machine learning"],
@@ -9,7 +9,7 @@ export const dsJobsDrives = [
   },
   {
     "id": 2,
-    "jobCompanyLogo": "https://www.odinschool.com/hubfs/OS_Company_Logo/v2/100_X_40/Fi.Money%20100X40.webp",
+    "jobCompanyLogo": "https://strapi.odinschool.com/uploads/Fi_Money_20100_X40_82839d0a1d.webp",
     "jobDesignation": "Product Analyst",
     "jobExp": "Fresher",
     "jobSkills": ["SQL", "Python", "Power BI"],
@@ -17,7 +17,7 @@ export const dsJobsDrives = [
   },
   {
     "id": 3,
-    "jobCompanyLogo": "https://www.odinschool.com/hubfs/OS_Company_Logo/100_X_40/Swan-Techpro-100X40.webp",
+    "jobCompanyLogo": "https://strapi.odinschool.com/uploads/Swan_Techpro_100_X40_e057777005.webp",
     "jobDesignation": "Junior Data Engineer",
     "jobExp": "Fresher & Experienced",
     "jobSkills": ["SQL", "Python"],
@@ -25,7 +25,7 @@ export const dsJobsDrives = [
   },
   {
     "id": 4,
-    "jobCompanyLogo": "https://www.odinschool.com/hubfs/Kriya%20Nextwealth.svg",
+    "jobCompanyLogo": "https://strapi.odinschool.com/uploads/Kriya_20_Nextwealth_7e10836c4b.svg",
     "jobDesignation": "Junior Business Analyst",
     "jobExp": "Experienced",
     "jobSkills": ["SQL", "Power BI", "Python", "Excel"],
@@ -41,7 +41,7 @@ export const dsJobsDrives = [
   },
   {
     "id": 6,
-    "jobCompanyLogo": "https://www.odinschool.com/hubfs/Logos.svg",
+    "jobCompanyLogo": "https://strapi.odinschool.com/uploads/Celebal_20_Logo_09cb6854b9.svg",
     "jobDesignation": "Junior Consultant - Data Scientist",
     "jobExp": "Fresher",
     "jobSkills": ["SQL", "Python", "Power BI"],
@@ -49,7 +49,7 @@ export const dsJobsDrives = [
   },
   {
     "id": 7,
-    "jobCompanyLogo": "https://www.odinschool.com/hubfs/Insyght.svg",
+    "jobCompanyLogo": "https://strapi.odinschool.com/uploads/Insyght_694bf5082c.svg",
     "jobDesignation": "Business Analyst",
     "jobExp": "Fresher and Experienced",
     "jobSkills": ["SQL", "Python", "Excel"],
@@ -65,7 +65,7 @@ export const dsJobsDrives = [
   },
   {
     "id": 9,
-    "jobCompanyLogo": "https://www.odinschool.com/hubfs/Kriya%20Nextwealth.svg",
+    "jobCompanyLogo": "https://strapi.odinschool.com/uploads/vetic_a1d6263c84.svg",
     "jobDesignation": "Business Analyst",
     "jobExp": "Experienced",
     "jobSkills": ["SQL", "Power BI"],
@@ -73,7 +73,7 @@ export const dsJobsDrives = [
   },
   {
     "id": 10,
-    "jobCompanyLogo": "https://www.odinschool.com/hubfs/Prudent.svg",
+    "jobCompanyLogo": "https://strapi.odinschool.com/uploads/Prudent_2e4c026b41.svg",
     "jobDesignation": "Data Scientist",
     "jobExp": "Freshers",
     "jobSkills": ["SQL", "Python"],
@@ -81,7 +81,7 @@ export const dsJobsDrives = [
   },
   {
     "id": 11,
-    "jobCompanyLogo": "https://www.odinschool.com/hubfs/Intellimark.svg",
+    "jobCompanyLogo": "https://strapi.odinschool.com/uploads/Intellimark_981eb529c8.svg",
     "jobDesignation": "Data Scientist",
     "jobExp": "Fresher & Experienced",
     "jobSkills": ["Python", "SQL", "Artificial Intelligence"],
@@ -97,7 +97,7 @@ export const dsJobsDrives = [
   },
   {
     "id": 13,
-    "jobCompanyLogo": "https://www.odinschool.com/hubfs/Randstad.svg",
+    "jobCompanyLogo": "https://strapi.odinschool.com/uploads/Flipkart_056ea5e304.svg",
     "jobDesignation": "Data Analyst",
     "jobExp": "Experienced",
     "jobSkills": ["Power BI", "SQL", "Excel"],
@@ -105,7 +105,7 @@ export const dsJobsDrives = [
   },
   {
     "id": 14,
-    "jobCompanyLogo": "https://www.odinschool.com/hubfs/Explainer%20Video%20Logos%20(1).svg",
+    "jobCompanyLogo": "https://strapi.odinschool.com/uploads/Explainer_20_Video_20_Logos_20_1_a19f76d6d6.svg",
     "jobDesignation": "Gallagher & Mohan",
     "jobExp": "Fresher",
     "jobSkills": ["Power BI", "Excel"],
@@ -113,7 +113,7 @@ export const dsJobsDrives = [
   },
   {
     "id": 15,
-    "jobCompanyLogo": "https://www.odinschool.com/hubfs/Explainer%20Video%20Logos%20(1)-1.svg",
+    "jobCompanyLogo": "https://strapi.odinschool.com/uploads/Explainer_20_Video_20_Logos_20_1_1_45438bb769.svg",
     "jobDesignation": "Product Data Analyst",
     "jobExp": "Fresher",
     "jobSkills": ["SQL", "Python"],
