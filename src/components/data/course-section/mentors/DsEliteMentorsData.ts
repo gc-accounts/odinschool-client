9ae4539d4096a023ffd7aa1efecfe3a8755a0031
Sheikh-Fayyaz-OdinSchool <fayyaz@odinschool.com>
2025-06-05 17:11:08 +0530

    auto-deploy

--- src/components/data/course-section/mentors/DsEliteMentorsData.ts
+++ src/components/data/course-section/mentors/DsEliteMentorsData.ts
@@ -1,78 +1,296 @@
 export const DsEliteMentorsData = [
   {
     "id": 1,
-    "name": "Dr. Gaurav Trivedi",
-    "photo": "https://strapi.odinschool.com/uploads/Dr_Gaurav_trivedi_1_98b932c680.webp",
-    "designation": "Professor",
-    "currentCompany": "https://strapi.odinschool.com/uploads/E_and_ICT_100_X40_3195d590ea.webp",
+    "name": "Sawinder Pal Kaur",
+    "photo": "https://strapi.odinschool.com/uploads/1_photo_c5f3a2bd58.jpg",
+    "designation": "Machine Learning Expert",
+    "currentCompany": "https://strapi.odinschool.com/uploads/1_current_company_bb97b6d4bb.jpg",
     "prevCompanies": [
-      "",
-      ""
+      "https://strapi.odinschool.com/uploads/1_prev_company_1_e81e7f3fe6.jpg",
+      "https://strapi.odinschool.com/uploads/1_prev_company_2_70ce1e5342.jpg"
     ]
   },
   {
     "id": 2,
-    "name": "Dr. Debanga Raj Neog",
-    "photo": "https://strapi.odinschool.com/uploads/Debanga_Raj_Neog_779f081433.webp",
-    "designation": "Assistant Professor",
-    "currentCompany": "https://strapi.odinschool.com/uploads/E_and_ICT_100_X40_3195d590ea.webp",
+    "name": "Dr. Subarna Roy",
+    "photo": "https://strapi.odinschool.com/uploads/2_photo_cba34e853b.jpg",
+    "designation": "Chief Data Scientist",
+    "currentCompany": "https://strapi.odinschool.com/uploads/2_current_company_64039aab61.jpg",
     "prevCompanies": [
-      "",
-      ""
+      "https://strapi.odinschool.com/uploads/2_prev_company_1_5345b688f0.jpg",
+      "https://strapi.odinschool.com/uploads/2_prev_company_2_1cce1ae249.jpg"
     ]
   },
   {
     "id": 3,
-    "name": "Dr. B S A S Rajita",
-    "photo": "https://strapi.odinschool.com/uploads/Dr_Rajita_2_61f77e14ce.webp",
-    "designation": "Lead Faculty",
-    "currentCompany": "hhttps://strapi.odinschool.com/uploads/odinschool_logo_8a880e4c88.webp",
+    "name": "Amit Rustagi",
+    "photo": "https://strapi.odinschool.com/uploads/3_photo_7d7d9a408b.jpg",
+    "designation": "Data Analytics Manager",
+    "currentCompany": "https://strapi.odinschool.com/uploads/3_current_company_4b52aa51b6.jpg",
     "prevCompanies": [
-      "",
-      ""
+      "https://strapi.odinschool.com/uploads/3_prev_company_1_9caf7ba8d0.jpg",
+      "https://strapi.odinschool.com/uploads/3_prev_company_2_bed86bd8e1.jpg"
     ]
   },
   {
     "id": 4,
-    "name": "Sandeep Singh",
-    "photo": "https://strapi.odinschool.com/uploads/sandeep_s_4b18ef4f79.webp",
-    "designation": "Lead Faculty",
-    "currentCompany": "https://strapi.odinschool.com/uploads/odinschool_logo_8a880e4c88.webp",
+    "name": "Rajeswaran V.",
+    "photo": "https://strapi.odinschool.com/uploads/4_photo_9903fb15f1.jpg",
+    "designation": "Senior Director",
+    "currentCompany": "https://strapi.odinschool.com/uploads/4_current_company_de4b5362ff.jpg",
     "prevCompanies": [
-      "",
-      ""
+      "https://strapi.odinschool.com/uploads/4_prev_company_1_8b36ffbb52.jpg",
+      "https://strapi.odinschool.com/uploads/4_prev_company_2_31d3551cd7.jpg"
     ]
   },
   {
     "id": 5,
-    "name": "Dr. Deepthi Chimala",
-    "photo": "https://strapi.odinschool.com/uploads/Deepthi_Chimala_e7b50ec04c.webp",
-    "designation": "Head, Carbon and Biodiversity",
-    "currentCompany": "https://strapi.odinschool.com/uploads/Arkadiah_Technology_100_X40_bbf86835d2.webp",
+    "name": "Teja Gudluru",
+    "photo": "https://strapi.odinschool.com/uploads/5_photo_b9c8fd785f.jpg",
+    "designation": "Founder",
+    "currentCompany": "https://strapi.odinschool.com/uploads/5_current_company_df76b768c0.jpg",
     "prevCompanies": [
-      "",
-      ""
+      "https://strapi.odinschool.com/uploads/5_prev_company_1_6d4a4c8d91.jpg",
+      "https://strapi.odinschool.com/uploads/5_prev_company_2_4f789b6c58.jpg"
     ]
   },
   {
     "id": 6,
-    "name": "Sangamesh KS",
-    "photo": "https://strapi.odinschool.com/uploads/Sangamesh_KS_b967d95229.webp",
-    "designation": "Founder",
-    "currentCompany": "https://strapi.odinschool.com/uploads/KSAC_200x80_afc88d18a4.webp",
+    "name": "S. Sai Subrath",
+    "photo": "https://strapi.odinschool.com/uploads/6_photo_9a5f299227.jpg",
+    "designation": "Senior Executive - HR",
+    "currentCompany": "https://strapi.odinschool.com/uploads/6_current_company_9e1a7516fe.jpg",
     "prevCompanies": [
-      "",
-      ""
+      "https://strapi.odinschool.com/uploads/6_prev_company_1_b1cb6b5a14.jpg",
+      "https://strapi.odinschool.com/uploads/6_prev_company_2_9e0c1c070f.jpg"
     ]
   },
   {
     "id": 7,
-    "name": "Naval Yemul",
-    "photo": "https://strapi.odinschool.com/uploads/Naval_Yemul_581a520c97.webp",
-    "designation": "Databricks Instructor",
-    "currentCompany": "https://strapi.odinschool.com/uploads/Data_Bricks_a93eba7aba.webp",
+    "name": "Abhishek Kumar",
+    "photo": "https://strapi.odinschool.com/uploads/7_photo_55d5b136cd.jpg",
+    "designation": "Director - Data Science",
+    "currentCompany": "https://strapi.odinschool.com/uploads/7_current_company_ad8fd7aff4.jpg",
     "prevCompanies": [
-      ""
+      "https://strapi.odinschool.com/uploads/7_prev_company_1_80be67eab9.jpg"
     ]
   },
+  {
+    "id": 8,
+    "name": "Akhilesh Chander",
+    "photo": "https://strapi.odinschool.com/uploads/8_photo_4259c47703.jpg",
+    "designation": "Ex - Data Science Manager",
+    "currentCompany": "https://strapi.odinschool.com/uploads/8_current_company_ab4b947ae4.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/8_prev_company_1_7ce9cc7bd3.jpg",
+      "https://strapi.odinschool.com/uploads/8_prev_company_2_d5dbc6202f.jpg"
+    ]
+  },
+  {
+    "id": 9,
+    "name": "Manish Kumar Singh",
+    "photo": "https://strapi.odinschool.com/uploads/9_photo_1cb9ba94bd.jpg",
+    "designation": "Data Scientist",
+    "currentCompany": "https://strapi.odinschool.com/uploads/9_current_company_098f6f8bbb.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/9_prev_company_1_8024c0fdec.jpg",
+      "https://strapi.odinschool.com/uploads/9_prev_company_2_43f1cf09d3.jpg"
+    ]
+  },
+  {
+    "id": 10,
+    "name": "Krishna Chaitanya",
+    "photo": "https://strapi.odinschool.com/uploads/10_photo_039cbd11d1.jpgp",
+    "designation": "Senior Solutions Architect",
+    "currentCompany": "https://strapi.odinschool.com/uploads/10_current_company_8a04b46300.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/10_prev_company_1_a3824df331.jpg",
+      "https://strapi.odinschool.com/uploads/10_prev_company_2_9313b4dbde.jpg"
+    ]
+  },
+  {
+    "id": 11,
+    "name": "Nagaraju Chikoti",
+    "photo": "https://strapi.odinschool.com/uploads/11_photo_6b09a92baf.jpg",
+    "designation": "Sr. Product Engineer",
+    "currentCompany": "https://strapi.odinschool.com/uploads/11_current_company_cd6f71ae3a.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/11_prev_company_1_6d5ff3e394.jpg",
+      "https://strapi.odinschool.com/uploads/11_prev_company_2_60ee4fcf49.jpg"
+    ]
+  },
+  {
+    "id": 12,
+    "name": "Roopak Prajapat",
+    "photo": "https://strapi.odinschool.com/uploads/12_photo_ec992b514c.jpg",
+    "designation": "Vice President",
+    "currentCompany": "https://strapi.odinschool.com/uploads/12_current_company_ce75d82542.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/12_prev_company_1_5f4e71b300.jpg",
+      "https://strapi.odinschool.com/uploads/12_prev_company_2_616e398190.jpg"
+    ]
+  },
+  {
+    "id": 13,
+    "name": "Aakash Kathuria",
+    "photo": "https://strapi.odinschool.com/uploads/13_photo_f2c7db20c7.jpg",
+    "designation": "General Manager",
+    "currentCompany": "https://strapi.odinschool.com/uploads/13_current_company_556e07e474.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/13_prev_company_1_bcdbe69cca.jpg",
+      "https://strapi.odinschool.com/uploads/13_prev_company_2_4167c7e124.jpg"
+    ]
+  },
+  {
+    "id": 14,
+    "name": "Saurabh Ray",
+    "photo": "https://strapi.odinschool.com/uploads/14_current_company_48b2f2d48c.jpg",
+    "designation": "Data Scientist",
+    "currentCompany": "https://strapi.odinschool.com/uploads/14_current_company_48b2f2d48c.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/14_prev_company_1_e21c39b596.jpg",
+      "https://strapi.odinschool.com/uploads/14_prev_company_2_fcd69818c3.jpg"
+    ]
+  },
+  {
+    "id": 15,
+    "name": "Dr. Chiranjiv Roy",
+    "photo": "https://strapi.odinschool.com/uploads/15_photo_db55dd2f9e.jpg",
+    "designation": "VP, Data Science",
+    "currentCompany": "https://strapi.odinschool.com/uploads/15_current_company_3119027d72.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/15_prev_company_1_0697ec282c.jpg",
+      "https://strapi.odinschool.com/uploads/15_prev_company_2_7d34ba4560.jpg"
+    ]
+  },
+  {
+    "id": 16,
+    "name": "Dr. Debojyoti Roy",
+    "photo": "https://strapi.odinschool.com/uploads/16_photo_7232a8bd8d.jpg",
+    "designation": "Chief Data Scientist",
+    "currentCompany": "https://strapi.odinschool.com/uploads/16_current_company_c4bacca45a.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/16_prev_company_1_a0c376cbf7.jpg"
+    ]
+  },
+  {
+    "id": 17,
+    "name": "Sarveshwaran Rajagopal",
+    "photo": "https://strapi.odinschool.com/uploads/17_photo_e35410e0e6.jpg",
+    "designation": "Sr. Data Scientist",
+    "currentCompany": "https://strapi.odinschool.com/uploads/17_current_company_88d8eb764e.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/17_prev_company_1_f8d777bb51.jpg"
+    ]
+  },
+  {
+    "id": 18,
+    "name": "Krishna Kumar",
+    "photo": "https://strapi.odinschool.com/uploads/18_photo_4814c350ff.jpg",
+    "designation": "Principal Data Scientist",
+    "currentCompany": "https://strapi.odinschool.com/uploads/18_current_company_269ac71e56.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/18_prev_company_1_158fadd778.jpg",
+      "https://strapi.odinschool.com/uploads/18_prev_company_2_c67226255a.jpg"
+    ]
+  },
+  {
+    "id": 19,
+    "name": "Arka Prava",
+    "photo": "https://strapi.odinschool.com/uploads/19_photo_622e3a1dd2.jpg",
+    "designation": "Lead Data Scientist",
+    "currentCompany": "https://strapi.odinschool.com/uploads/19_current_company_c931af5298.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/19_prev_company_1_52da3768cc.jpg",
+      "https://strapi.odinschool.com/uploads/19_prev_company_2_17b997648c.jpg"
+    ]
+  },
+  {
+    "id": 20,
+    "name": "Vishal Jain",
+    "photo": "https://strapi.odinschool.com/uploads/20_photo_c8fa0df27c.jpg",
+    "designation": "PDM",
+    "currentCompany": "https://strapi.odinschool.com/uploads/20_current_company_2911fb0b75.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/20_prev_company_1_6196cd4614.jpg",
+      "https://strapi.odinschool.com/uploads/20_prev_company_2_8c76c93aac.jpg"
+    ]
+  },
+  {
+    "id": 21,
+    "name": "Rahul Saha",
+    "photo": "https://strapi.odinschool.com/uploads/21_photo_8c7f10a545.jpg",
+    "designation": "Chief Data Scientist",
+    "currentCompany": "https://strapi.odinschool.com/uploads/21_current_company_575f3fad4d.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/21_prev_company_1_40038fc28a.jpg",
+      "https://strapi.odinschool.com/uploads/21_prev_company_2_74542eab1f.jpg"
+    ]
+  },
+  {
+    "id": 22,
+    "name": "Anand Narayanan",
+    "photo": "https://strapi.odinschool.com/uploads/22_photo_9b820c6e1a.jpg",
+    "designation": "Founder",
+    "currentCompany": "https://strapi.odinschool.com/uploads/22_current_company_5356c25209.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/22_prev_company_1_6c63771584.jpg",
+      "https://strapi.odinschool.com/uploads/22_prev_company_2_fbb5db821c.jpg"
+    ]
+  },
+  {
+    "id": 23,
+    "name": "Murlidhar Bhekane",
+    "photo": "https://strapi.odinschool.com/uploads/23_photo_f4e0c4dd26.jpg",
+    "designation": "Staff Data Engineer",
+    "currentCompany": "https://strapi.odinschool.com/uploads/23_current_company_07833f0b9d.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/23_prev_company_1_6e118ae3b6.jpg",
+      "https://strapi.odinschool.com/uploads/23_prev_company_2_0b9213ce21.jpg"
+    ]
+  },
+  {
+    "id": 24,
+    "name": "Avinash Mishra",
+    "photo": "https://strapi.odinschool.com/uploads/24_photo_5c0f5dad54.jpg",
+    "designation": "Data Science Manager",
+    "currentCompany": "https://strapi.odinschool.com/uploads/24_current_company_7ab38ffd57.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/24_prev_company_1_80501f6f98.jpg",
+      "https://strapi.odinschool.com/uploads/24_prev_company_2_3546645872.jpg"
+    ]
+  },
+  {
+    "id": 25,
+    "name": "Abidunnisa Begum",
+    "photo": "https://strapi.odinschool.com/uploads/25_photo_0fc565c879.jpg",
+    "designation": "Lead Instructor",
+    "currentCompany": "https://strapi.odinschool.com/uploads/odinschool_logo_8a880e4c88.webp",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/25_prev_company_1_2d9246063b.jpg",
+      "https://strapi.odinschool.com/uploads/25_prev_company_2_fca5b0496a.jpg"
+    ]
+  },
+  {
+    "id": 26,
+    "name": "Jyant Mahara",
+    "photo": "https://strapi.odinschool.com/uploads/26_photo_9aec7780aa.jpg",
+    "designation": "Data Science Lead",
+    "currentCompany": "https://strapi.odinschool.com/uploads/26_current_company_c6982ad690.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/26_prev_company_1_848a8f56bd.jpg",
+      "https://strapi.odinschool.com/uploads/26_prev_company_2_2e858fe050.jpg"
+    ]
+  },
+  {
+    "id": 27,
+    "name": "Sasha Kingsley",
+    "photo": "https://strapi.odinschool.com/uploads/27_photo_24f4b46131.jpg",
+    "designation": "Analytics Manager",
+    "currentCompany": "https://strapi.odinschool.com/uploads/27_current_company_9f77c3f127.jpg",
+    "prevCompanies": [
+      "https://strapi.odinschool.com/uploads/27_prev_company_1_6980512dae.jpg",
+      "https://strapi.odinschool.com/uploads/27_prev_company_2_25dce2e11e.jpg"
+    ]
+  }
 ];
