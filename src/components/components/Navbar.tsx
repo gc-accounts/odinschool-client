d63025fd19e155656a9063deca578f75934e4aa4
Sheikh-Fayyaz-OdinSchool <fayyaz@odinschool.com>
2025-06-05 17:42:57 +0530

    auto-deploy

--- src/components/components/Navbar.tsx
+++ src/components/components/Navbar.tsx
@@ -7,11 +7,20 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <header className="md:px-0 px-4 sticky top-0 z-40 w-full bg-white backdrop-blur supports-[backdrop-filter]:bg-white shadow-[0_4px_12px_rgba(0,0,0,0.05)]">
-      <div className="container flex h-16 items-center justify-between px-[30px]">
+    <header className="md:px-0 px-4 sticky top-0 z-40 w-full bg-primary-50 backdrop-blur supports-[backdrop-filter]:bg-primary-100 shadow-[0_4px_12px_rgba(0,0,0,0.05)]">
+      <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-2">
           <MobileNav />
           <MainNav />
+        </div>
+        <div className="flex items-center gap-2">
+          <Link href="/" className="mr-6 flex items-center">
+        <img
+          src="https://strapi.odinschool.com/uploads/odinschool_logo_8a880e4c88.webp" // Replace with your actual logo URL
+          alt="EduPlatform Logo"
+          className="h-8 w-auto"
+        />
+      </Link>
         </div>
         <div className="flex items-center gap-2">
           <Button className="bg-primary text-white hover:bg-primary/90">
