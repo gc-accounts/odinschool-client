db6c21530ad1b6450d1838b8c0479f9b4f9f9098
gc-accounts <jatin@odinschool.com>
2025-06-05 18:10:21 +0530

    added padding on navbar

--- src/components/components/Navbar.tsx
+++ src/components/components/Navbar.tsx
@@ -8,19 +8,19 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <header className="md:px-0 px-4 sticky top-0 z-40 w-full md:bg-white bg-primary-50 backdrop-blur supports-[backdrop-filter]:bg-primary-100 shadow-[0_4px_12px_rgba(0,0,0,0.05)]">
-      <div className="container flex h-16 items-center justify-between">
+      <div className="container flex h-16 items-center justify-between md:px-[30px] px-0">
         <div className="flex items-center gap-2">
           <MobileNav />
           <MainNav />
         </div>
         <div className="md:hidden flex items-center gap-2">
           <Link href="/" className="mr-6 flex items-center">
-        <img
-          src="https://strapi.odinschool.com/uploads/odinschool_logo_8a880e4c88.webp" // Replace with your actual logo URL
-          alt="EduPlatform Logo"
-          className="h-8 w-auto"
-        />
-      </Link>
+            <img
+              src="https://strapi.odinschool.com/uploads/odinschool_logo_8a880e4c88.webp" // Replace with your actual logo URL
+              alt="EduPlatform Logo"
+              className="h-8 w-auto"
+            />
+          </Link>
         </div>
         <div className="flex items-center gap-2">
           <Button className="bg-primary text-white hover:bg-primary/90">
